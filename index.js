15b892c3ec688d46fa61966cb4cfc2910bcc3c6f
aidyn1984 <71663287+aidyn1984@users.noreply.github.com>
2021-04-26 20:18:30 -0500

    commit tree ec9753f62a1eee9b18e5b3a422b200a7c25e37f4

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(array,string) {
+    let card = []
+    for (let i = 0; i < array.length; i++) {
+        card.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`)
+    }
+
+    return card;
+}
+
+function countDown(params) {
+    while ( params > 0 ) {
+        console.log(params);
+        params -= 1;
+    }
+    console.log(params);
+
+}
+
